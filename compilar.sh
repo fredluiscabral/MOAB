4b9e07d8e5d9d2ea78a9c5ab9920acdbdb0a665e
Frederico Luis Cabral <fredluiscabral@gmail.com>
2024-03-08 01:31:07 -0300

    compilar.sh

---
+++ compilar.sh
@@ -0,0 +1,2 @@
+g++ -std=c++11 -O3 -I/usr/include/eigen3 -I/prj/prjad/fcabral/Doutorado/MOAB -I/opt/intel/oneapi/mkl/2024.0/include /prj/prjad/fcabral/Doutorado/MOAB/bural.c laicoMult.cpp -L/opt/intel/oneapi/mkl/2024.0/lib/intel64 -Wl,-rpath,/opt/intel/oneapi/mkl/2024.0/lib/intel64 -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl -fopenmp -DARMA_DONT_USE_WRAPPER -o laicoMult
+
